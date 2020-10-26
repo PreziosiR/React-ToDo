73f8602e7a710c34bf5979246f65b621cddf6cfa
PreziosiR <rpreziosi00@gmail.com>
2020-10-26 15:02:44 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import ToDoList from './ToDoList'
+import NavBar from './NavBar'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import AddTask from './AddTask'
+import initialData from '../initialData'
+import uniqid from 'uniqid'
+
+class App extends React.Component{
+
+    state = {
+        tasks: initialData
+    }
+
+    onToggleCompleted = (taskId) => {
+        let taskToUpdate = this.state.tasks.find(task => task.id === taskId)
+        taskToUpdate.completed = !taskToUpdate.completed
+
+        this.setState(prevState => {
+            prevState.tasks.map(task => {
+                return task.id === taskId ? taskToUpdate : task
+            })
+        })
+    }
+
+    onAddTask = (newTaskName) => {
+        let newTask = {
+            id: uniqid(),
+            name: newTaskName,
+            completed: false
+        }
+
+        this.setState(prevState => ({
+            tasks: [...prevState.tasks, newTask]
+        }))
+    }
+
+    render(){
+        return(
+            <section id="todo">
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/add-task" render={(props) => <AddTask {...props} onAddTask={this.onAddTask} />} />
+                        <Route path="/:filter?" render={(props) => <ToDoList {...props} tasks={initialData} onToggleCompleted={this.onToggleCompleted}/>} />
+                    </Switch>
+                    <NavBar />
+                </BrowserRouter>
+            </section>
+        )
+    }
+}
+
+export default App
